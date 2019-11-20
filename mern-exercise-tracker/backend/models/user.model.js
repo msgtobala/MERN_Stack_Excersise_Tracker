37d02892355c0038e7e8502db62097716e1b5e5d
Balaji Venkatraman <balaji@moonraft.com>
2019-11-20 19:08:44 +0530

    added schemas

--- mern-exercise-tracker/backend/models/user.model.js
+++ mern-exercise-tracker/backend/models/user.model.js
@@ -17,6 +17,6 @@ const userSchema = new Schema(
   }
 );
 
- const User = mongoose.model('User', userSchema);
- 
- module.exports = User;
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
